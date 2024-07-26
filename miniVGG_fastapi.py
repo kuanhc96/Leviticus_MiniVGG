dcc264bd9ed787094a920472479d27df2cb1faae
kuanhc96 <kuanhc96@uchicago.edu>
2024-07-26 12:15:19 +0800

    access second item in model.evaluate

--- miniVGG_fastapi.py
+++ miniVGG_fastapi.py
@@ -148,7 +148,7 @@ def train(request: MiniVGGTrainRequest) -> dict:
         uniqueLabels = np.unique(labels)
 
     print("[INFO] Scoring Model")
-    accuracy = model.evaluate(testImages, testLabels)
+    accuracy = model.evaluate(testImages, testLabels, batch_size=32)[1]
     classificationReport = classification_report(testLabels.argmax(axis=1), predictions.argmax(axis=1), labels=uniqueLabels)
     print("[INFO] Train Request Complete, Returning Training Results")
 
